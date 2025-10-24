1bc68acf148bd399860e155c7ce940df2a7ceb10
Zarina138 <zrinaabdulazizova@gmail.com>
2025-10-24 17:07:50 +0000

    Task

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+
+    if number > 0:
+        print(number % 10)
+    elif number < 0:
+        print((number * (-1)) % 10)
+    else:
+        print("0")
