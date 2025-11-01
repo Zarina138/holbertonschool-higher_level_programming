099f5761a4f417fd8c1ffb0a5321616b0267bf71
Zarina138 <zrinaabdulazizova@gmail.com>
2025-11-01 15:34:59 +0000

    Task

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x**2, row)) for row in matrix]
+def search_replace(my_list, search, replace):
+    new_list = []
+    for x in my_list:
+        if x == search:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+    return new_list
