792f809d01abc713bc4add5665e128da4a2717df
Zarina138 <zrinaabdulazizova@gmail.com>
2025-11-05 20:37:23 +0000

    Task

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        divde = a / b
+        divide = a / b
     except (TypeError, ZeroDivisionError):
         divide = None
     finally:
-        print("Result: {}".format(divide))
+        print("Inside result: {}".format(divide))
     return (divide)
