b926c72d48e68d6821cfdb5142e4426c3f760ab9
Zarina138 <zrinaabdulazizova@gmail.com>
2025-11-07 17:44:06 +0000

    Task

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,8 +8,8 @@ Floats are first casted to integers before addition.
 
 def add_integer(a, b=98):
     """Add two integers or floats and return the result as integer."""
-    if not isinstance(a, (int, float)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return (int(a) + int(b))
