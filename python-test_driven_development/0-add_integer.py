d1da045bb9c39a461f8491a1bcf3bdc195758bd9
Zarina138 <zrinaabdulazizova@gmail.com>
2025-11-07 17:32:32 +0000

    Task

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    try:
+        a = int(a)
+    except Exception:
+        raise TypeError("a must be an integer")
+
+    try:
+        b = int(b)
+    except Exception:
+        raise TypeError("b must be an integer")
+
+    return a + b
