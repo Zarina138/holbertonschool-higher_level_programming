4757da8f7af3bc854041462fd6c9d492528c8cf9
Zarina138 <zrinaabdulazizova@gmail.com>
2025-11-07 17:52:39 +0000

    Task

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,9 +5,9 @@ It ensures inputs are either integers or floats.
 If inputs are invalid, a TypeError is raised.
 Floats are first casted to integers before addition.
 """
+
+
 def add_integer(a, b=98):
-
-
     """Add two integers or floats and return the result as integer."""
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
