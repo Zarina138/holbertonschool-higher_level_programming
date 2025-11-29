a5388e383536cc9a94da94e2a19c0e5eeeaf50ef
Zarina138 <zrinaabdulazizova@gmail.com>
2025-11-29 15:45:04 +0000

    Task-SQL

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- taskkkkkk
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES states(id),
