e8b081b0cb091beee8b7762c9b975261c574ab3b
Zarina138 <zrinaabdulazizova@gmail.com>
2025-12-05 12:24:58 +0000

    PYTHON-ORM

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a script that lists all states with a name starting with N from the database 
+a script that lists all states with a name starting with N from the database
 """
 import sys
 import MySQLdb
