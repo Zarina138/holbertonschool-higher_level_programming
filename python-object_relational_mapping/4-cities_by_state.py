f1b87dc0912d1244a0c2fa00a22310458f8ef247
Zarina138 <zrinaabdulazizova@gmail.com>
2025-12-05 15:29:01 +0000

    Python -ORM

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     user_name = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    
+
     db = MySQLdb.connect(
         user=user_name,
         passwd=password,
@@ -20,10 +20,10 @@ if __name__ == "__main__":
 
     curs = db.cursor()
     curs.execute(
-    "SELECT cities.id, cities.name, states.name "
-    "FROM cities "
-    "JOIN states ON cities.state_id = states.id "
-    "ORDER BY cities.id ASC"
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
 )
 
     rows = curs.fetchall()
