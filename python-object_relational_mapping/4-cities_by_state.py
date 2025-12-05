63267263448a116064134046f0dec654f696e3bb
Zarina138 <zrinaabdulazizova@gmail.com>
2025-12-05 15:16:29 +0000

    Python -ORM

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+ a script that lists all cities from the database
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+db = MySQLdb.connect(
+    user = user_name,
+    passwd = password,
+    db = db_name,
+    host = "localhost",
+    port = 3306
+
+)
+
+curs = db.cursor()
+curs.execute("SELECT * FROM cities ORDER BY ASC")
+
+rows = db.fetchall()
+for row in rows():
+    print(row)
+
+curs.close()
+db.close()
