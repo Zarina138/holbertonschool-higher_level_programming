488d9e44bce6eeb4caa45edf09ca7561ddb0dfc4
Zarina138 <zrinaabdulazizova@gmail.com>
2025-12-05 15:19:10 +0000

    Python -ORM

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,15 +9,14 @@ if __name__ == "__main__":
     user_name = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-
-db = MySQLdb.connect(
-    user = user_name,
-    passwd = password,
-    db = db_name,
-    host = "localhost",
-    port = 3306
-
-)
+    
+    db = MySQLdb.connect(
+        user=user_name,
+        passwd=password,
+        db=db_name,
+        host="localhost",
+        port=3306
+    )
 
 curs = db.cursor()
 curs.execute("SELECT * FROM cities ORDER BY ASC")
