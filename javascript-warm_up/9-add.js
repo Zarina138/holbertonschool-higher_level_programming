791e7a67030bf1c48dc1a5b0047f2e992a281d63
Zarina138 <zrinaabdulazizova@gmail.com>
2025-12-27 18:53:31 +0000

    task completed

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 function add (a, b) {
-    return a + b;
-  }
-  
-  const first = parseInt(process.argv[2]);
-  const second = parseInt(process.argv[3]);
-  
-  console.log(add(first, second));
-  
+  return a + b;
+}
+ 
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+
+console.log(add(first, second));
