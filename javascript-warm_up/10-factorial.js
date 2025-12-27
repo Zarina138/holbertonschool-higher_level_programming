7c81cff8969e5af3d3e2e788b5321764ffbeec04
Zarina138 <zrinaabdulazizova@gmail.com>
2025-12-27 18:55:46 +0000

    task completed

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function factorial (n) {
-    if (isNaN(n) || n <= 0) {
+  if (isNaN(n) || n <= 0) {
       return 1;
-    }
-    return n * factorial(n - 1);
-  }
+}
+  return n * factorial(n - 1);
+}
   
-  const num = parseInt(process.argv[2]);
-  console.log(factorial(num));
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
